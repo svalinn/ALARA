437dae649c671eed7baafef3d16775c668cd800f
wilsonp <wilsonp@3ca0fc55-144d-4cef-8816-fa8a2f6afc42>
2002-04-16 20:21:41 +0000

    Changed comment and fixed conflict.

--- src/alara.h
+++ src/alara.h
@@ -1,4 +1,4 @@
-/* $Id: alara.h,v 1.10 2002-04-16 20:16:17 wilsonp Exp $ */
+/* $Id: alara.h,v 1.11 2002-04-16 20:21:41 wilsonp Exp $ */
 #ifndef _ALARA_H
 #define _ALARA_H 1
 
@@ -22,7 +22,7 @@
 
 #include <map>
 
-/* TO DO: Talk to Tim about STL under Solaris!!  Why won't it compile?!?!?*/
+/* TO DO: Come see me next week  Why won't it compile?!?!?*/
 
 typedef map<int,double,less<int> > DataCache;
 typedef map<int,double*,less<int> > VectorCache;
