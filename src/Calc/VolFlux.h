2e161252db8912e01fae4ab74a0a7281fa445035
fateneja <fateneja@3ca0fc55-144d-4cef-8816-fa8a2f6afc42>
2002-08-23 20:45:31 +0000

    Converted to doxygen comments. Implemented direct rtflux reading

--- src/Calc/VolFlux.h
+++ src/Calc/VolFlux.h
@@ -1,122 +1,5 @@
-/* $Id: VolFlux.h,v 1.11 2002-05-06 18:04:40 wilsonp Exp $ */
+/* $Id: VolFlux.h,v 1.12 2002-08-23 20:45:31 fateneja Exp $ */
 #include "alara.h"
-
-/* ******* Class Description ************
-
-This class stores the information about the neutron fluxes in each
-interval.  Each Volume object will have a linked list of these VolFlux
-objects, with each element in the list representing a different flux
-specification.
-
- *** Static Class Members ***
-
- nFluxes : int
-    The number of user-defined Flux specifications.
-
- nGroups : int
-    The number of neutron groups being used in this problem.
-
- refflux_type : int
-    An indicator of which type of reference flux should be used for
-    truncation calculations.
-
- *** Non-Static Class Members ***
- 
- flux : double*
-    An array of scalar flux values.
-
- cache : RateCache*
-    A data cache to prevent refolding the cross-sections with the
-    fluxes too often.  (see RateCache)
-
- next : VolFlux*
-    A pointer to the next VolFlux object in this list.
-
- *** Static Member Functions ***
- 
- * - Utility - *
-
- void setNumFluxes(int)
-    Inline function sets the number of fluxes.
-
- static int getNumFluxes()
-    Inline function queries the number of fluxes.
-
- static void setNumGroups(int)
-    Inline function sets the number of neutron groups.
-
- static int getNumGroups() 
-    Inline function queries the number of neutron groups.
-
- static void setRefFluxType(char)
-    Inline function sets the type of reference flux to use.
-
- static int getRefFluxType()
-    Inline function quesries the reference flux type.
-
- *** Member Functions ***
-
- * - Constructors & Destructors - *
-
- VolFlux()
-    Default constructor creates storage for 'flux' if 'nGroups'>0,
-    otherwise sets 'flux' to NULL.  Always sets 'next' to NULL.
-
- VolFlux(const VolFlux&)
-    Copy constructor copies 'flux' on element-by-element basis.  Sets
-    next to NULL.
-
- VolFlux(ifstream &, double )
-    This constructor reads the flux values from a file attached to the
-    first argument stream reference and scales them by the second
-    argument.
-
- ~VolFlux()
-    Inline destructor delets storage for 'flux' and destroys list of
-    VolFlux objects by deleting 'next'.
-
- VolFlux& operator=(const VolFlux&)
-    The correct implementation of this operator must ensure that
-    previously allocated space is returned to the free store before
-    allocating new space into which to copy the object. Note that
-    'next' is NOT copied, the object will continue to be part of the
-    same list unless explicitly changed.
-
-
- * - Input - *
-
- VolFlux* read(ifstream &, double )
-    This function extends the list of flux info by passing the
-    arguments to the VolFlux(ifstream&,double) constructor.  The newly
-    created object is pointed to by the 'next' of the object through
-    which the function is called and a pointer to the newly created
-    object is returned.
- 
- * - Solution - *
-
- double updateReference(VolFlux*,double)
-    This function compares the flux of the object through which it is
-    called with the object pointed to by the argument, and sets the
-    the reference flux. There are currently two options:
-    1) group-wise maximum flux, or
-    2) group-wise volume weighted average flux
-
- void scale(double)
-    This function scales the fluxes but multiplying by the first argument.
-
- double fold(double*,Node*)
-    This function takes a rate vector pointed to by the first argument
-    and folds it with this flux, returning the scalar reaction rate.
-    The second argument points to the Node object associated with this
-    rate vector and will be used to determine the indexing
-    information for the RateCache (see RateCache).
-
- * - Utility - *
-
- VolFlux* advance()
-    Inline function provides access to the 'next' object in the list.
-
- */
 
 #ifndef _VOLFLUX_H
 #define _VOLFLUX_H
@@ -126,26 +9,59 @@ specification.
 
 #include "RateCache.h"
 
+/** \brief This class stores the information about the neutron fluxes in 
+ *         each interval.  
+ *         
+ *  Each Volume object will have a linked list of these VolFlux
+ *  objects, with each element in the list representing a different flux
+ *  specification.
+ */
+
 class VolFlux
 {
 protected:
-  static int nFluxes, nGroups, refflux_type;
+  static int 
+    /// The number of user-defined Flux specifications.
+    nFluxes, 
 
-  double *flux;
-  RateCache cache;
+    /// The number of neutron groups being used in this problem.
+    nGroups, 
+    
+    /// An indicator of which type of reference flux should be used for
+    /// truncation calculations.
+    refflux_type;
 
-  VolFlux *next;
+  double 
+    /// An array of scalar flux values.
+    *flux;
+
+  RateCache 
+    /// A data cache to prevent refolding the cross-sections with the
+    /// fluxes too often.  (see RateCache)
+    cache;
+
+  VolFlux 
+    /// A pointer to the next VolFlux object in this list.
+    *next;
 
 public:
-  /* Utility */
+  /// Inline function sets the number of fluxes.
   static void setNumFluxes(int numFlx)
     { nFluxes = numFlx; };
+
+  /// Inline function queries the number of fluxes.
   static int getNumFluxes() 
     { return nFluxes; };
+  
+  /// Inline function sets the number of neutron groups.
   static void setNumGroups(int numGrps)
     { nGroups = numGrps; };
+
+  /// Inline function queries the number of neutron groups.
   static int getNumGroups() 
     { return nGroups; };
+  
+  /// Inline function sets the type of reference flux to use.
   static void setRefFluxType(char refflux_type_code)
     { 
       switch (refflux_type_code) {
@@ -158,27 +74,73 @@ public:
 	break;
       }
     };
+
+  /// Inline function queries the reference flux type.
   static int getRefFluxType()
     { return refflux_type; } ;
 
-  /* Service */
+  /// Default Constructor
+  /** This constructor creates storage for 'flux' if 'nGroups'>0,
+    otherwise sets 'flux' to NULL.  Always sets 'next' to NULL. */
   VolFlux();
+
+  /// Copy constructor 
+  /** This constructor copies 'flux' on element-by-element basis.  Sets
+      next to NULL. */
   VolFlux(const VolFlux&);
+
+  /// This constructor reads the flux data from an array.
+  /** It takes the values in the array and stores them in VolFlux::Flux */  
+  VolFlux(double*, double);
+  
+  /// This constructor reads the flux values from a file attached to the
+  /// first argument stream reference and scales them by the second
+  /// argument.
   VolFlux(ifstream &, double );
+   
+  
+  /** Inline destructor deletes storage for 'flux' and destroys list of
+       VolFlux objects by deleting 'next'. */
   ~VolFlux()
     { delete flux; delete next; };
 
+  /// Overloaded assignment operator
+  /** The correct implementation of this operator must ensure that
+      previously allocated space is returned to the free store before
+      allocating new space into which to copy the object. Note that
+      'next' is NOT copied, the object will continue to be part of the
+      same list unless explicitly changed. */
   VolFlux& operator=(const VolFlux&);
 
-  /* Input */
+  /// This function extends the list of flux info by passing the
+  /// arguments to the VolFlux(ifstream&,double) constructor.  
+  /** The newly created object is pointed to by the 'next' of the 
+      object through which the function is called and a pointer to the
+      newly created object is returned. */
   VolFlux* read(ifstream &, double );
-  
-  /* Solution */
+
+  VolFlux* copyData(double *, double);
+
+  /// This function compares the flux of the object through which it is
+  /// called with the object pointed to by the argument, and sets the
+  /// the reference flux. 
+  /** There are currently two options:
+        1) group-wise maximum flux, or
+        2) group-wise volume weighted average flux */
   void updateReference(VolFlux*,double);
+
+  /// This function scales the fluxes but multiplying by the first 
+  /// argument.
   void scale(double);
+  
+  /// This function takes a rate vector pointed to by the first argument
+  /// and folds it with this flux, returning the scalar reaction rate.
+  /** The second argument points to the Node object associated with this
+      rate vector and will be used to determine the indexing
+      information for the RateCache (see RateCache). */
   double fold(double*,Node*);
 
-  /* Utility */
+  /// Inline function provides access to the 'next' object in the list.
   VolFlux* advance() {return next;};
 
 };
