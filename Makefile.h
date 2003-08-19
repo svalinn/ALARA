df6358d65eb28b0d043f87ea8acdd128edd6e140
wilsonp <wilsonp@3ca0fc55-144d-4cef-8816-fa8a2f6afc42>
2003-08-19 22:17:22 +0000

    Added Makefile header that has some additional but universally required new installation directory definitions.

---
+++ Makefile.h
@@ -0,0 +1,7 @@
+#### These things will be included in all necessary Makefile.am files
+#### in this hierarchy
+
+# where to put documentation, data and sample input/output
+docdir = $(pkgdatadir)/doc
+xsdir = $(pkgdatadir)/data
+sampledir = $(pkgdatadir)/samples
