493a35cdf6b1ee132f29215f420a6589d7ca5828
Eitan Weinstein <esweinstein2@tux-117.cae.wisc.edu>
2024-06-19 11:26:49 -0500

    Expanding logger to capture 'No bottleneck testing available' message

--- src/DataLib/fendl32B_retrofit/logging_config.py
+++ src/DataLib/fendl32B_retrofit/logging_config.py
@@ -1,12 +1,28 @@
 # Import packages
 import logging
+import sys
 
 # Configure logging
 logging.basicConfig(
     level= 'INFO',
-    format='%(asctime)s - %(levelname)s - %(message)s',
+    format= '%(asctime)s - %(levelname)s - %(message)s',
     filename= 'fendl3_gendf.log',
     filemode= 'w'
 )
 
 logger = logging.getLogger(__name__)
+
+# Redirect stdout and stderr to the logger
+class LoggerWriter:
+    def __init__(self, level):
+        self.level = level
+
+    def write(self, message):
+        if message.strip():  # Avoid logging empty messages
+            self.level(message.strip())
+
+    def flush(self):
+        pass  # No need to flush, but method must be defined
+
+sys.stdout = LoggerWriter(logger.info)
+sys.stderr = LoggerWriter(logger.error)
