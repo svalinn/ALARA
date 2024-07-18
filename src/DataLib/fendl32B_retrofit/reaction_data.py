1e40d6f6e4c8bf2e61574e6d28c43f41de872541
Eitan Weinstein <esweinstein2@tux-116.cae.wisc.edu>
2024-07-18 12:47:57 -0500

    First commit for counting emitted particles by reaction.

--- src/DataLib/fendl32B_retrofit/reaction_data.py
+++ src/DataLib/fendl32B_retrofit/reaction_data.py
@@ -14,6 +14,67 @@ NP_dict   = {'n'     : array([-1      ,      0      ]), # neutron emission
              'γ'     : array([ 0      ,      0      ])  # gamma emission
 }
 
+def count_emitted_particles(particle, emitted_particle_string):
+    """
+    Count emitted particles from a reaction given a target particle
+        and the particles produced in a neutron activation reaction.
+
+    Arguments:
+        particle (str): Name of the target particle produced in the reaction.
+            Options include n, p, alpha, d, t, and 3He, corresponding to
+            neutrons, protons, alpha particles, deuterons, tritons,
+            and helium-3 nuclides.
+        emitted_particle_string (str): Particle product(s) of the neutron
+            activation, of the format 'p' for a single proton for example or
+            '2n' for two neutrons etc.
+
+    Returns:
+        particle_count (int or None): Count of the target particle present in
+            the product. If particles not present, returns None rather than 0.
+    """
+
+    particle_index = emitted_particle_string.find(particle)
+    number_str = ''
+    for i in range(particle_index - 1, -1, -1):
+        if emitted_particle_string[i].isdigit():
+            number_str = emitted_particle_string[i] + number_str
+        else:
+            break
+
+    if number_str:
+        particle_count = int(number_str)
+    elif particle in emitted_particle_string:
+        particle_count = 1
+    else:
+        particle_count = None
+    
+    return particle_count
+
+def emission_breakdown(emitted_particles):
+    """
+    Identify the particles and their respective counts from a particular
+        nuclear decay in response to neutron activation. These tallies are
+        saved in a dictionary with the name of the particles as the keys.
+
+    Arguments:
+        emitted_particles (str): Particle product(s) of a neutron activation
+            reaction formatted in a singular string (i.e. np for a 
+            (neutron, proton) emission.
+
+    Returns:
+        emission_dict (dict): Dictionary containing each individual particle
+            type and their respetive counts. For an np emission, this would
+            read out as {'n': 1, 'p': 1}.
+    """
+
+    emission_dict = {
+        particle : count_emitted_particles(particle, emitted_particles)
+        for particle in NP_dict.keys()
+        if particle in emitted_particles
+    }
+    
+    return emission_dict
+
 def nucleon_changes(emission_dict):
     """
     Calculate the change in neutrons and protons in a nucleus in response to
