4e5168fd0f89f34886c18e509e7f9042c189fdf6
Eitan Weinstein <esweinstein2@tux-116.cae.wisc.edu>
2024-07-18 15:42:37 -0500

    Improving readability of test parameters.

--- src/DataLib/fendl32B_retrofit/test_reaction_data.py
+++ src/DataLib/fendl32B_retrofit/test_reaction_data.py
@@ -4,16 +4,16 @@ import pytest
 import reaction_data as rxd
 
 @pytest.mark.parametrize(
-    "particle, emitted_particle_string, exp",
+        "(particle, emitted_particle_string, exp)",
     [
-        ('n', 'np', 1),
-        ('p', 'np', 1),
-        ('t', 'np', 0),
-        ('n', '357n14d', 357),
-        ('p', '357n14d', 0),
-        ('d', '357n14d', 14),
-        ('3He', '3He8α', 1),
-        ('α', '3He8α', 8)
+         ('n'     , 'np'                   , 1  ) ,
+        ('p'      , 'np'                   , 1  ) ,
+        ('t'      , 'np'                   , 0  ) ,
+        ('n'      , '357n14d'              , 357) ,
+        ('p'      , '357n14d'              , 0  ) ,
+        ('d'      , '357n14d'              , 14 ) ,
+        ('3He'    , '3He8α'                , 1  ) ,
+        ('α'      , '3He8α'                , 8  )
     ]
 )
 def test_count_emitted_particles(particle, emitted_particle_string, exp):
