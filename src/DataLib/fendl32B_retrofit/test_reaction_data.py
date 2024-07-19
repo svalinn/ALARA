00fa8bb35f833c47adcc7075a8c2b449bb9935e2
Eitan Weinstein <esweinstein2@cnerg-docker-04.neep.wisc.edu>
2024-07-19 14:18:18 -0500

    Fixing test script.

--- src/DataLib/fendl32B_retrofit/test_reaction_data.py
+++ src/DataLib/fendl32B_retrofit/test_reaction_data.py
@@ -1,11 +1,11 @@
-
 # Import packages
 import pytest
 import reaction_data as rxd
 from numpy import array
+from numpy.testing import assert_array_equal
 
 @pytest.mark.parametrize(
-        "(particle, emitted_particle_string, exp)",
+        "particle, emitted_particle_string, exp",
     [
         ('n'      , 'np'                   , 1  ) ,
         ('p'      , 'np'                   , 1  ) ,
@@ -22,13 +22,13 @@ def test_count_emitted_particles(particle, emitted_particle_string, exp):
     assert obs == exp
 
 @pytest.mark.parametrize(
-    "(emitted_particles, exp)",
+         "emitted_particles  , exp"                          ,
     [
-        ('np' , {'n' : 1, 'p' : 1}),
-        ('2d3t3He', {'d' : 2, 't' : 3, '3He' : 1}),
-        ('total', {}),
-        ('α75γn3', {'α' : 1, 'γ': 75, 'n': 1})
-        ('Xα', {})
+        ('np'                ,            {'n' : 1, 'p' : 1}),
+        ('2d3t3He'           , {'d' : 2, 't' : 3, '3He' : 1}),
+        ('total'             , {}                           ),
+        ('α75γn3'             , {'α' : 1, 'γ': 75, 'n': 1}   ), 
+        ('Xα'                 , {}                          )
     ]
 )
 def test_emission_breakdown(emitted_particles, exp):
@@ -37,17 +37,17 @@ def test_emission_breakdown(emitted_particles, exp):
 
 
 @pytest.mark.parametrize(
-    "(emission_dict, exp)",
+          "emission_dict                 , exp",
     [
-        ({'n' : 1 , 'p' : 1} , array([ 1 , -1])),
-        ({'t' : 2 , 'n' : 1, '3He' : 3} , array([-7 , -8])),
-        ({'α' : 1 , 'γ' : 4, 'd': 1} , array([-2 , -3])) ,
-        ({'n' : 1 , 'p' : 1, 'd' : 1 , 't' : 1 ,
-          '3He' : 1 , 'α' : 1 ,'γ' : 1} , array([-6 , -7])),
-        ({}, array([None, None]))
+        ({'n' : 1 , 'p' : 1}             , array([ 0 , -1])) ,
+        ({'t' : 2 , 'n' : 1 , '3He' : 3} , array([-7 , -8])) ,
+        ({'α' : 1 , 'γ' : 4, 'd': 1}     , array([-2 , -3])) ,
+        ({'n' : 1 , 'p' : 1, 'd' : 1     ,
+          't' : 1 , '3He' : 1 , 'α' : 1  ,
+          'γ' : 1}                       , array([-6 , -7])) ,
+        ({}                              , array([None, None]))
     ]
 )
 def test_nucleon_changes(emission_dict, exp):
     obs = rxd.nucleon_changes(emission_dict)
-    assert obs == exp
-
+    assert_array_equal(obs, exp)
