935f2d8c1c34b83958ecd1524019944ac885debf
Eitan Weinstein <esweinstein2@tux-106.cae.wisc.edu>
2024-07-19 12:13:46 -0500

    First commit for MT data preprocessing with tests.

--- src/DataLib/fendl32B_retrofit/test_reaction_data.py
+++ src/DataLib/fendl32B_retrofit/test_reaction_data.py
@@ -1,8 +1,6 @@
 # Import packages
 import pytest
 import reaction_data as rxd
-from numpy import array
-from numpy.testing import assert_array_equal
 
 @pytest.mark.parametrize(
         "particle, emitted_particle_string, exp",
@@ -22,32 +20,41 @@ def test_count_emitted_particles(particle, emitted_particle_string, exp):
     assert obs == exp
 
 @pytest.mark.parametrize(
-         "emitted_particles  , exp"                          ,
+    "(mt_data_csv, exp)",
     [
-        ('np'                ,            {'n' : 1, 'p' : 1}),
-        ('2d3t3He'           , {'d' : 2, 't' : 3, '3He' : 1}),
-        ('total'             , {}                           ),
-        ('α75γn3'             , {'α' : 1, 'γ': 75, 'n': 1}   ), 
-        ('Xα'                 , {}                          )
+        ('mt_test1.csv', {'11'                 : 
+                          {'Reaction'          :   '(z,2nd)'   ,
+                           'delKZA'            :      -10030   ,
+                           'Emitted Particles' :       '2nd'
+                           }})                                 ,
+        ('mt_test2.csv', {'1'                  :
+                          {'Reaction'          : '(n,total)'   ,
+                           'delKZA'            :       'N/A'   , 
+                           'Emitted Particles' :     'total' 
+                           }})                                 ,
+        ('mt_test3.csv', {'30'                 :
+                          {'Reaction'          :  '(z,2n2α)'   ,
+                           'delKZA'            :      -40090   ,
+                           'Emitted Particles' :      '2n2α'
+                           }})                                 ,
+        ('mt_test4.csv', {'106'                : 
+                          {'Reaction'          :   '(z,3He)'   ,
+                           'delKZA'            :      -20020   ,
+                           'Emitted Particles' :       '3He'
+                           }})                                 ,
+        ('mt_test5.csv', {'207'                :
+                          {'Reaction'          :    '(z,Xα)'   ,
+                           'delKZA'            :       'N/A'   ,
+                           'Emitted Particles' :        'Xα'   
+                          }})                                  ,
+        ('mt_test6.csv', {'700'                :
+                          {'Reaction'          :    '(z,t1)'   ,
+                           'delKZA'            :      -20019   ,
+                           'Emitted Particles' :          't'
+                           }})
     ]
 )
-def test_emission_breakdown(emitted_particles, exp):
-    obs = rxd.emission_breakdown(emitted_particles)
+def test_process_mt_data(mt_data_csv, exp):
+    dir = './files_for_tests'
+    obs = rxd.process_mt_data(rxd.load_mt_table(f'{dir}/{mt_data_csv}'))
     assert obs == exp
-
-
-@pytest.mark.parametrize(
-          "emission_dict                 , exp",
-    [
-        ({'n' : 1 , 'p' : 1}             , array([ 0 , -1])) ,
-        ({'t' : 2 , 'n' : 1 , '3He' : 3} , array([-7 , -8])) ,
-        ({'α' : 1 , 'γ' : 4, 'd': 1}     , array([-2 , -3])) ,
-        ({'n' : 1 , 'p' : 1, 'd' : 1     ,
-          't' : 1 , '3He' : 1 , 'α' : 1  ,
-          'γ' : 1}                       , array([-6 , -7])) ,
-        ({}                              , array([None, None]))
-    ]
-)
-def test_nucleon_changes(emission_dict, exp):
-    obs = rxd.nucleon_changes(emission_dict)
-    assert_array_equal(obs, exp)
