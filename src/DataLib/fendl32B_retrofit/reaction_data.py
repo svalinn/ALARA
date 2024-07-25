822ecb8dc4dc997b1b0fa2ec6d248943d5be0dcc
Eitan Weinstein <esweinstein2@cnerg-docker-04.neep.wisc.edu>
2024-07-25 09:59:31 -0500

    Improving emission_breakdown() efficiency.

--- src/DataLib/fendl32B_retrofit/reaction_data.py
+++ src/DataLib/fendl32B_retrofit/reaction_data.py
@@ -75,8 +75,8 @@ def emission_breakdown(emitted_particles):
     emission_dict = {
         particle: count_emitted_particles(particle, emitted_particles)
         for particle in NP_dict.keys()
-        if particle in emitted_particles and all(
-            spec_case not in emitted_particles for spec_case in spec_reactions
+        if particle in emitted_particles and not any(
+            spec_case in emitted_particles for spec_case in spec_reactions
         )
     }
     
