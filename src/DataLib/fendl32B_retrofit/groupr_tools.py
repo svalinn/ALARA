4eb0cef11a7e0fe68d3e76eee796fb383d1b1d7d
Eitan Weinstein <esweinstein2@cnerg-docker-04.neep.wisc.edu>
2024-07-25 15:42:45 -0500

    First commit for template substitution.

--- src/DataLib/fendl32B_retrofit/groupr_tools.py
+++ src/DataLib/fendl32B_retrofit/groupr_tools.py
@@ -30,6 +30,23 @@ cards = {
     10 :                                                 [MATD]
 }
 
+# Dictionary of elements in the Periodic Table
+elements = [
+    'H', 'He', 'Li', 'Be', 'B', 'C', 'N', 'O', 'F', 'Ne',
+    'Na', 'Mg', 'Al', 'Si', 'P', 'S', 'Cl', 'Ar', 'K', 'Ca',
+    'Sc', 'Ti', 'V', 'Cr', 'Mn', 'Fe', 'Co', 'Ni', 'Cu', 'Zn',
+    'Ga', 'Ge', 'As', 'Se', 'Br', 'Kr', 'Rb', 'Sr', 'Y', 'Zr',
+    'Nb', 'Mo', 'Tc', 'Ru', 'Rh', 'Pd', 'Ag', 'Cd', 'In', 'Sn',
+    'Sb', 'Te', 'I', 'Xe', 'Cs', 'Ba', 'La', 'Ce', 'Pr', 'Nd',
+    'Pm', 'Sm', 'Eu', 'Gd', 'Tb', 'Dy', 'Ho', 'Er', 'Tm', 'Yb',
+    'Lu', 'Hf', 'Ta', 'W', 'Re', 'Os', 'Ir', 'Pt', 'Au', 'Hg',
+    'Tl', 'Pb', 'Bi', 'Po', 'At', 'Rn', 'Fr', 'Ra', 'Ac', 'Th',
+    'Pa', 'U', 'Np', 'Pu', 'Am', 'Cm', 'Bk', 'Cf', 'Es', 'Fm',
+    'Md', 'No', 'Lr', 'Rf', 'Db', 'Sg', 'Bh', 'Hs', 'Mt', 'Ds',
+    'Rg', 'Cn', 'Nh', 'Fl', 'Mc', 'Lv', 'Ts', 'Og'
+]
+elements = dict(zip(elements, range(1, len(elements)+1)))
+
 def format_card(card_number, card_content):
     """
     Format individual "cards" for the NJOY input cards to run with GROUPR.
@@ -50,16 +67,13 @@ def format_card(card_number, card_content):
     # Initialize string and concatenate contents of the card with it
     card_str = ''
     gen_str = ' ' + ' '.join(map(str, card_content))
-    if card_number == 9:
-        card_str = ' ' + '/\n '.join(card_content) + '/\n'
-    elif card_number == 4:
+    if card_number == 4:
         card_str += gen_str + '\n'
     else:
         card_str += gen_str + '/\n'
     return card_str
 
 def establish_static_template():
-
     """
     Formulate a static string that serves as a template for an NJOY input file
         used to run the GROUPR module. The filled-in parameters are predefined
@@ -85,3 +99,55 @@ def establish_static_template():
     input_template = Template(input_string)
 
     return input_template
+
+def fill_input_template(material_id, MTs, element, A, mt_dict, template): 
+    """
+    Substitute in the material-specific values for a given ENDF/PENDF file
+        into the template for the NJOY input card. These values are the
+        material ID, the title, which incorporates the isotopic description,
+        and the reactions corresponding to the MT numbers encoded in the
+        files.
+
+    Arguments:
+        material_id (int): Unique material identifier, defined by the ENDF-6
+            Formats Manual
+            (https://www.oecd-nea.org/dbdata/data/manual-endf/endf102.pdf).
+        MTs (list of int): List of reaction types (MT's) present in the
+            ENDF/PENDF files.
+        element (str): Chemical symbol for element of interest.
+        A (str or int): Mass number for selected isotope.
+            If the target is a metastable isomer, "m" is written after the
+            mass number, so A must be input as a string.
+        mt_dict (dict): Reference dictionary containing reaction information
+            for each MT number pre-defined in the ENDF manual.
+        template (string.Template): A static string template containing
+            the general structure of the NJOY input file to run GROUPR,
+            $identifiers for substitutable items, produced by executing
+            establish_static_template().
+    
+    Returns:
+        template (string.Template): Modified template with the material-
+            specific information substituted in for the $identifiers.
+    """
+
+    Z = str(elements[element]).zfill(2)
+    title = f'"{Z}-{element}-{A} for TENDL 2017"'
+
+    card9_lines = []
+    for i, MT in enumerate(MTs):
+        mtname = mt_dict[str(MT)]['Reaction']
+        card9_line = f'3 {MT} "{mtname}" '
+        
+        # Include a "/" at the end of each line except for the last line,
+        # which will already have one automatically from the template
+        if i != len(MTs) - 1:
+            card9_line += '/'
+        card9_lines.append(card9_line)
+    
+    card9 = '\n '.join(card9_lines)
+    
+    return template.substitute(
+        mat_id = material_id,
+        title = title,
+        reactions = card9
+    )
