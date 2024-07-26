419a96d96ad15f69c28d23dada10d257857e44dc
Eitan Weinstein <esweinstein2@cnerg-docker-04.neep.wisc.edu>
2024-07-26 10:11:30 -0500

    Minor reformat of comments.

--- src/DataLib/fendl32B_retrofit/groupr_tools.py
+++ src/DataLib/fendl32B_retrofit/groupr_tools.py
@@ -2,22 +2,22 @@
 from string import Template
 
 # Define constants
-NENDF = 20 # unit for endf tape
-NPEND = 21 # unit for pendf tape
-NGOUT1 = 0 # unit for input gout tape (default=0)
-NGOUT2 = 31 # unit for output gout tape (default=0)
-IGN = 17 # neutron group structure option (corresponding to Vitamin J)
-IGG = 0 # gamma group structure option
-IWT = 11 # weight function option (corresponding to Vitamin E)
-LORD = 0 # Legendre order
-NTEMP = 1 # number of temperatures (default=1)
-NSIGZ = 1 # number of sigma zeroes (default=1)
-IPRINT = 1 # long print option (0/1=minimum/maximum) --(default=1)
-ISMOOTH = 1 # switch on/off smoother operation (1/0, default=1=on)
-TEMP = 293.16 # temperature in Kelvin
-SIGZ = 0 # sigma zero values (including infinity)
-MFD = 3 # file to be processed
-MATD = 0 # next mat number to be processed
+NENDF = 20                                               # unit for endf tape
+NPEND = 21                                              # unit for pendf tape
+NGOUT1 = 0                             # unit for input gout tape (default=0)
+NGOUT2 = 31                           # unit for output gout tape (default=0)
+IGN = 17        # neutron group structure option (corresponding to Vitamin J)
+IGG = 0                                        # gamma group structure option
+IWT = 11                # weight function option (corresponding to Vitamin E)
+LORD = 0                                                     # Legendre order
+NTEMP = 1                                # number of temperatures (default=1)
+NSIGZ = 1                                # number of sigma zeroes (default=1)
+IPRINT = 1            # long print option (0/1=minimum/maximum) --(default=1)
+ISMOOTH = 1            # switch on/off smoother operation (1/0, default=1=on)
+TEMP = 293.16                                         # temperature in Kelvin
+SIGZ = 0                             # sigma zero values (including infinity)
+MFD = 3                                                # file to be processed
+MATD = 0                                    # next mat number to be processed
 
 # Define a dictionary template for GROUPR
 cards = {
