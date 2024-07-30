5f54aac28e57976c19243560bd3b499a351590ec
Eitan Weinstein <esweinstein2@cnerg-docker-04.neep.wisc.edu>
2024-07-30 10:14:58 -0500

    Simplifying testing.

--- src/DataLib/fendl32B_retrofit/test_tendl_preprocessing.py
+++ src/DataLib/fendl32B_retrofit/test_tendl_preprocessing.py
@@ -2,6 +2,8 @@
 import pytest
 import tendl_preprocessing as tpp
 
+dir = './files_for_tests'
+
 @pytest.mark.parametrize(
     "endf_file, exp",
     [
@@ -11,28 +13,12 @@ import tendl_preprocessing as tpp
            45, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66,
            67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 91, 102,
            103, 104, 105, 106, 107, 108, 111, 112, 115, 116, 117]]),
-
-        ('endf_test2.tendl',
-         [125,
-          [1, 2, 102]]),
-        
-        ('endf_test3.tendl',
-         [325,
-          [1, 2, 4, 24, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63,
-           64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80,
-           81, 102, 103, 105]]),
-
-        ('endf_test4.tendl',
-         [9228,
-          [1, 2, 4, 5, 16, 17, 18, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61,
-           62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78,
-           79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 91, 102, 649, 800, 801,
-           802, 803, 804, 805, 806, 807, 808, 809, 810, 811, 812, 813, 814,
-           815, 816, 817, 818, 819, 820, 821, 822, 823, 824, 825, 826, 827,
-           828, 829, 830, 831, 832, 833, 834, 835]])
     ]
 )
 def test_extract_endf_specs(endf_file, exp):
-    dir = './files_for_tests'
     obs = tpp.extract_endf_specs(f'{dir}/{endf_file}')
     assert obs == exp
+
+def test_extract_endf_specs_empty_endf():
+    with pytest.raises(Exception, match='std::exception'):
+        tpp.extract_endf_specs(f'{dir}/endf_test2.tendl')
