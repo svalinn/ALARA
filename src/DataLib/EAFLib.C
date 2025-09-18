b65882165913bd181b2216b1323eba9a1bb3ed0f
Eitan Shai Weinstein <esweinstein2@tux-118.cae.wisc.edu>
2025-09-18 10:33:04 -0500

    ALARAJOY memory tweaks, one more FENDL rename.

--- src/DataLib/EAFLib.C
+++ src/DataLib/EAFLib.C
@@ -51,7 +51,7 @@ EAFLib::~EAFLib()
 
 void EAFLib::extract(char* input, float* value)
 {
-  char section[128];
+  char section[64];
 
   /* find beginning of exponent */
   int expStart = strcspn(input,"+-");
