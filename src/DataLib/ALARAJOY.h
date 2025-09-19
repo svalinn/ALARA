2696a3eee055d2dd297aba8b95bf89e6c3a768ce
Eitan Shai Weinstein <esweinstein2@tux-119.cae.wisc.edu>
2025-09-19 11:39:13 -0500

    Removing nGroups hardcoding.

--- src/DataLib/ALARAJOY.h
+++ src/DataLib/ALARAJOY.h
@@ -82,6 +82,12 @@ class ALARAJOYLib : public EAFLib
         size_t currentRowIndex;
         int currentParent;
 
+        // DSV functions
+        DSVRow parseDSVRow(const std::string& line);
+        std::string cleanXSectionString(std::stringstream& ss);
+        std::vector<float> parseXSectionArray(const std::string& arrayStr);
+        void loadDSVData();
+        
 };
 
 #endif
