4bdad786457260446a9ca27a8a9652a15f1ddcf1
Anupama Shashikala Rajendra <asrajendra@cnerg-docker-02.neep.wisc.edu>
2025-11-07 14:36:16 -0600

    Use string.Template to manage text replacement

--- examples/20y_all_nuc/all_nuc_inp.py
+++ examples/20y_all_nuc/all_nuc_inp.py
@@ -1,10 +1,11 @@
 import argparse
 import yaml
+import string
 
 # This script produces the ALARA input file 20y_all_nuc.inp in this directory, with a different element library and some formatting
 # differences that may need to be addressed manually
 
-def read_nuclib(nuclib, template_alara_inp):
+def read_nuclib(nuclib, old_inp_name):
     nuclib_lines = open(nuclib, 'r').readlines()  
     return nuclib_lines
 
@@ -22,32 +23,38 @@ def make_volume_block(nuclib_lines, volume):
 
     return vol_list, load_list, mix_list  
 
-def edit_template(vol_list, load_list, mix_list, template_alara_inp):
+def edit_template(vol_list, load_list, mix_list, old_inp_name, new_inp_name):
     # Uses an existing ALARA input file (in this case ../singleElement_20y_inp/alara_inp_fe_20y) and appends all lines
     # starting from the material_lib line to a new input file.
     blocks = ['volume', 'mat_loading', 'mixture']
     line_lists = [vol_list, load_list, mix_list]
-    new_inp = open('20y_all_nuc_test.inp', 'w')
-    new_inp.write('geometry rectangular \n \n')
+    new_lines = ""
+    new_lines += 'geometry rectangular \n \n'
+    
     for block_id, block in enumerate(line_lists):
         if block_id != 2:
-            new_inp.write(f'{blocks[block_id]}\n')
-        for line in line_lists[block_id]:       
-            new_inp.write(line)
+            new_lines += f'{blocks[block_id]}\n'
+        for line in line_lists[block_id]:      
+            new_lines += line
         if block_id != 2:    
-            new_inp.write('end \n')   
+            new_lines += 'end \n' 
 
-    with open(template_alara_inp, 'r') as temp_file:
-        inside_block = False
-        for temp_line in temp_file:
-            stripped_line = temp_line.strip()
-            if stripped_line.startswith('material_lib'):
-                inside_block = True
-                new_inp.write(temp_line)
-                continue
-            if inside_block == True:
-                new_inp.write(temp_line)
-                
+    new_inp = open(new_inp_name, 'w+')
+    old_inp = open(old_inp_name, 'r+')
+
+    #Prepare to use old ALARA input file as template onto which new input lines are added
+    for old_line in old_inp:
+        if old_line.strip().startswith('material_lib'):
+            break
+        else:
+            old_line = ""
+    old_inp.seek(0)
+    old_inp.write('${placeholder}')
+    
+    new_inp.write(new_lines)
+    template = string.Template(old_inp.read())        
+    new_out = template.substitute(placeholder = new_lines)
+    new_inp.write(new_out)
 
 #Define all inputs and execute functions:
 #---------------------------------------------------------------------------------------------
@@ -68,13 +75,14 @@ def main():
     params = read_yaml(args)
 
     nuclib = params['nuclib']
-    template_alara_inp = params['template_alara_inp']
+    old_inp_name = params['old_inp_name']
+    new_inp_name = params['new_inp_name']
     volume = params['volume']
 
-    nuclib_lines= read_nuclib(nuclib, template_alara_inp)
+    nuclib_lines = read_nuclib(nuclib, old_inp_name)
     vol_list, load_list, mix_list = make_volume_block(nuclib_lines, volume)
 
-    edit_template(vol_list, load_list, mix_list, template_alara_inp)
+    edit_template(vol_list, load_list, mix_list, old_inp_name, new_inp_name)
 
 if __name__ == '__main__':
     main()    
