f7d85b5ea592ae4a01d8ba2293d96aa22b68b32b
Eitan Shai Weinstein <esweinstein2@tux-107.cae.wisc.edu>
2025-11-21 07:35:28 -0600

    Follow up to last review

--- tools/alara_output_processing/alara_output_processing.py
+++ tools/alara_output_processing/alara_output_processing.py
@@ -3,13 +3,120 @@ import re
 import argparse
 from io import StringIO
 from warnings import warn
+from numpy import array
+from csv import DictReader
+
+# ---------- General Utility Methods ----------
+
+SECONDS_CONV = {'s': 1}
+SECONDS_CONV['m'] = 60  * SECONDS_CONV['s']
+SECONDS_CONV['h'] = 60  * SECONDS_CONV['m']
+SECONDS_CONV['d'] = 24  * SECONDS_CONV['h']
+SECONDS_CONV['w'] = 7   * SECONDS_CONV['d']
+SECONDS_CONV['y'] = 365 * SECONDS_CONV['d']
+SECONDS_CONV['c'] = 100 * SECONDS_CONV['y']
+
+def convert_times(vector, from_unit, to_unit):
+    '''
+    Convert the values in a vector to and from any units in the list of
+        seconds (s), minutes (m), hours (h), days (d), weeks (w), years (y),
+        or centuries (c).
+
+    Arguments:
+        vector (numpy.array): 1-D NumPy array containing time values.
+        from_unit (str): Time units of vector's input state. Accepted values:
+            's', 'm', 'h', 'd', 'w', 'y', 'c'.
+        to_unit (str): Time units of converted vector. Accepted values: 's',
+            'm', 'h', 'd', 'w', 'y', 'c'.
+
+    Returns:
+        converted_vector (numpy.ndarray): 1-D Numpy array of time values
+            converted to the desired units.
+    '''
+
+    return vector * (SECONDS_CONV[from_unit] / SECONDS_CONV[to_unit])
+
+def extract_time_vals(times, to_unit='s'):
+    '''
+    Return the cooling times of an ALARADFrame from their column headers
+        as a list in time units of the user's specification.
+
+    Arguments:
+        times (list): List of cooling times from an ALARA output table header.
+        to_unit (str, optional): Time units for conversion. Accepted values:
+            's', 'm', 'h', 'd', 'w', 'y', 'c'.
+            (Defaults to 's')
+
+    Returns:
+        times (numpy.ndarray): 1-D NumPy array of the converted ALARA cooling
+            times.
+    '''
+    
+    numeric_times = []
+    for time in times:
+        if 'shutdown' in time:
+            numeric_times.append(0.0)
+        else:
+            time_str, unit = time.split('_')
+            numeric_times.append(float(time_str))
+    
+    return convert_times(
+        array(numeric_times), from_unit=unit, to_unit=to_unit
+    )
+
+def aggregate_small_percentages(piv, relative=False, threshold=0.05):
+    '''
+    Consolidate all rows in a pre-filtered and pivoted DataFrame derived from
+        a larger ALARADFRame that do not have any cells with a contribution of
+        more than a threshold value to the total for its respective column.
+        Rows that do not have any cells that surpass its column's threshold
+        are aggregated into a new "Other" row. If a row has at least one
+        column value above the threshold, then the whole row is preserved.
+
+    Arguments:
+        piv (pd.DataFrame): DataFrame created from the pivoting of an
+            ALARADFrame already operated on by ALARADFrame.filter_rows().
+        relative (bool, optional): Option for ALARADFrames already
+            processed by relative_contributions().
+            (Defaults to False)     
+        threshold (float or int): Proportional threshold value for
+            inclusion cutoff.
+            (Defaults to 0.05)
+
+    Returns:
+        agg (alara_output_processing.ALARADFrame): Processed ALARADFrame
+            (potentially) with new row, "Other", containing all aggregated
+            data below the thresholds.
+    '''
+
+    piv = ALARADFrame(piv)
+    cols = piv.columns
+    
+    rel_adjustment = (
+        [1] * len(cols) if relative else piv.extract_totals()
+    )
+    threshold_vals = {
+        col: threshold * adj for col, adj in zip(cols, rel_adjustment)
+    }
+    piv = piv[piv.index != 'total']
+
+    small_mask = (piv[cols] < pd.Series(threshold_vals)).all(axis=1)
+    other_row = pd.Series(piv.loc[small_mask, cols].sum(), name='Other')
+    agg = piv.loc[~small_mask.values]
+
+    if other_row[cols].sum() > 0:
+        agg = pd.concat([agg, other_row.to_frame().T])
+    agg.index.name = 'nuclide'
+
+    return agg
+
+###########################################
 
 class FileParser:
 
     def __init__(self, filepath, run_lbl):
         self.filepath = filepath
         self.run_lbl = run_lbl
-        self.rows = []
         self.results = ALARADFrame()
 
     # ---------- Utility and Helper Methods ----------
@@ -65,8 +172,8 @@ class FileParser:
             current_block
     ):
         '''
-        Parse a block of table lines with StringIO into an ALARADFrame and
-            store each row as a single element in a list of rows.
+        Parse a block of table lines with StringIO into a csv.DictReader and
+            store each row as a dictionary in a list of rows.
         
         Arguments:
             self (alara_output_processing.FileParser): FileParser object.
@@ -93,27 +200,34 @@ class FileParser:
                 } 
         '''
 
-        current_table_rows = []
-        df = pd.read_csv(
-            StringIO('\n'.join(current_table_lines)), sep=r'\s+'
-        ).set_index('isotope')
+        # Prepare table text for csv.DictReader
+        text = '\n'.join(current_table_lines)
+        comma_delim = re.sub(r'\t+|\s{2,}', ',', text.strip())
+        
+        reader = DictReader(StringIO(comma_delim), delimiter=',')
+        
+        # Handle and reformat DictReader data for row writing
+        raw_cols = reader.fieldnames[:]
+        nuclide_col = raw_cols[0]
+        time_cols = raw_cols[1:]
 
-        df.columns = [c.replace('_', '') for c in df.columns]
+        converted_times = extract_time_vals(time_cols)
+        time_key_map = dict(zip(time_cols, converted_times))
+
         block_name, block_num_trail = current_block.split(' #')
         variable = current_parameter.split(' [')[0]
-        adf = ALARADFrame(df)
-        adf.columns = adf.extract_time_vals(parsing=True)
 
-        for isotope in adf.index:
-            for time in adf.columns:
+        current_table_rows = []
+        for row in reader:
+            for old_time, new_time in time_key_map.items():
                 current_table_rows.append({
-                    'time'                 :                             time,
-                    'nuclide'              :                          isotope,
-                    'run_lbl'              :                     self.run_lbl,
-                    'block'                :       adf.BLOCK_ENUM[block_name],
-                    'block_num'            :    block_num_trail.split(' ')[0],
-                    'variable'             :      adf.VARIABLE_ENUM[variable],
-                    'value'                :           adf.loc[isotope, time]
+                    'time'           :                               new_time,
+                    'nuclide'        :                       row[nuclide_col],
+                    'run_lbl'        :                           self.run_lbl,
+                    'block'          :     ALARADFrame.BLOCK_ENUM[block_name],
+                    'block_num'      :          block_num_trail.split(' ')[0],
+                    'variable'       :    ALARADFrame.VARIABLE_ENUM[variable],
+                    'value'          :           float(row.get(old_time, ''))
                 })
 
         return current_table_rows
@@ -140,6 +254,7 @@ class FileParser:
         current_block = None
         inside_table = False
         current_table_lines = []
+        rows = []
 
         for line in lines:
             line = line.strip()
@@ -164,7 +279,7 @@ class FileParser:
                 current_table_lines.append(line)
                 if self._is_end_of_table(line):
                     if current_parameter and current_block:
-                        self.rows.extend(self._parse_table_data(
+                        rows.extend(self._parse_table_data(
                             current_table_lines,
                             current_parameter,
                             current_block
@@ -172,8 +287,8 @@ class FileParser:
                     inside_table = False
                     current_table_lines = []
                 continue
-        
-        self.results = ALARADFrame(self.rows)
+
+        self.results = ALARADFrame(rows)
 
         if self.results.empty:
             warn(f'Unable to read tables from {self.filepath}')
@@ -197,25 +312,6 @@ class ALARADFrame(pd.DataFrame):
     @property
     def _constructor(self):
         return ALARADFrame
-    
-    def _operate_inplace(self, inplace=False):
-        '''
-        Internal method to establish whether subsequent ALARADFrame operations
-            act on the ALARADFrame itself or produce a copy frame.
-        
-        Arguments:
-            self (alara_output_processing.ALARADFrame): Specialized ALARA
-                output DataFrame.
-            inplace (bool, optional): Option to modify the ALARADFrame or
-                create a new one.
-
-        Returns:
-            adf (alara_output_processing.ALARADFrame): Either the original
-                ALARADFrame or a copy.
-
-        '''
-
-        return self if inplace else self.copy()
 
     def _filter_elements(self, elements):
         '''
@@ -237,7 +333,7 @@ class ALARADFrame(pd.DataFrame):
 
         return self[self['nuclide'].str.contains(regex, case=False, na=False)]
 
-    def filter_rows(self, filter_dict, inplace=False):
+    def filter_rows(self, filter_dict):
         '''
         Filter all rows that match user specifications for one or more
             columns.
@@ -253,9 +349,7 @@ class ALARADFrame(pd.DataFrame):
                     Column 1 : Value or [Value1, ..., Value N],
                     ...
                     Column N : Value or [Value1, ..., Value N]
-                } 
-            inplace (bool, optional): Option to modify the ALARADFrame or
-                create a new one.
+                }
 
         Returns:
             filtered_adf (alara_output_processing.ALARADFrame): Modified copy
@@ -263,88 +357,43 @@ class ALARADFrame(pd.DataFrame):
                 filter_dict.
         '''
         
-        filtered_adf = self._operate_inplace(inplace)
+        filtered_adf = self.copy()
         for col_name, filters in filter_dict.items():
-            if not isinstance(filters, list):
-                filters = [filters]
+            if col_name in filtered_adf.columns:
+                if not isinstance(filters, list):
+                    filters = [filters]
 
-            # Special wrapping of filter_elements() to select all nuclides of
-            # a given element or elements without having to explicitly list
-            # each nuclide present from that element or list of elements
-            if (
-                col_name == 'nuclide'
-                and any('-' not in f and f != 'total' for f in filters)
-            ):
-                    filtered_adf = filtered_adf._filter_elements(filters)
-                    continue
+                if col_name == 'nuclide':
+                    elements, specific_nuclides = [], []
+                    for f in filters:
+                        if '-' not in f and f != 'total':
+                            elements.append(f)
+                        else:
+                            specific_nuclides.append(f)
 
-            mask = filtered_adf[col_name].isin(filters)
-            filtered_adf = filtered_adf[mask]
-        
-        return filtered_adf.reset_index(drop=True)
+                    mask = pd.Series(False, index=filtered_adf.index)
 
-    def extract_time_vals(self, parsing=False):
-        '''
-        Return the cooling times of an ALARADFrame from their column headers
-            as a list in seconds.
+                    if elements:
+                        mask |= filtered_adf.index.isin(
+                            filtered_adf._filter_elements(elements).index
+                        )
 
-        Arguments:
-            self (alara_output_processing.ALARADFrame): Specialized ALARA
-                output DataFrame.
-            parsing (bool, optional): Option for ALARADFrame format when
-                calling this funcion inside FileParser._parse_table_data().
-                (Defaults to False)
+                    if specific_nuclides:
+                        mask |= filtered_adf[col_name].isin(specific_nuclides)
 
-        Returns:
-            times (list): List of the ALARA cooling times, written as 
-                floating point numbers of seconds.
-        '''
-
-        if not parsing:
-            return sorted(set(self['time']))
-    
-        else:
-            times = []
-            time_dict = { 'shutdown' : 0.0, 'y' : 365*24*60*60 }
-
-            for time in self.columns:
-                if time == 'shutdown':
-                    times.append(time_dict['shutdown'])
                 else:
-                    times.append(float(time.split('y')[0]) * time_dict['y'])
+                    mask = filtered_adf[col_name].isin(filters)
+
+                filtered_adf = filtered_adf[mask]
             
-            return times
-        
-    def convert_times(self, unit, inplace=False, pivot=False):
-        '''
-        Convert the values in the ALARADFrame rows or columns from seconds to
-            minutes (m), hours (h), days (d), weeks (w), years (y), or
-            centuries (c).
+            else:
+                warn(
+                    f'Column name "{col_name}" from filter_dict not found in ' \
+                    f'any of the ALARADFrame columns. Skipping filter for ' \
+                    f'{col_name}.'
+                )
 
-        Arguments:
-            self (alara_output_processing.ALARADFrame): ALARADFrame on which
-                to operate. If operating on a pivot table, make sure to
-                convert to an ALARADFrame first from pd.DataFrame.
-            unit (str): Time unit for time value conversion. Accepted values:
-                m, h, d, w, y, c.
-            inplace (bool, optional): Option to modify the ALARADFrame or
-                create a new one.
-            pivot (bool, optional): Option for operation on a pivot table.
-            (Defaults to False) 
-        '''
-
-        conv_dict = {'m' : 60}
-        conv_dict['h'] = 60  * conv_dict['m']
-        conv_dict['d'] = 24  * conv_dict['h']
-        conv_dict['w'] = 7   * conv_dict['d']
-        conv_dict['y'] = 365 * conv_dict['d']
-        conv_dict['c'] = 100 * conv_dict['y']
-
-        adf = self._operate_inplace(inplace)
-        times = adf.columns if pivot else adf['time']
-        times /= conv_dict[unit]
-
-        return adf
+        return filtered_adf.reset_index(drop=True)
 
     def extract_totals(self):
         '''
@@ -353,26 +402,24 @@ class ALARADFrame(pd.DataFrame):
             response variable.
         
         Arguments:
-            self (alara_output_processing.ALARADFrame): Specialized ALARA
-                output DataFrame.
+            self (alara_output_processing.ALARADFrame): Filtered ALARADFrame
+                with only one variable and one run or a pivoted ALARADFrame of
+                'nuclide' vs. 'time'.
 
         Returns:
-            totals (list of floats or None): List of total values at each
-                cooling time in the ALARADFrame. Returns empty list if 'total'
-                row not present in the ALARADFrame.
+            totals (list of floats): List of total values at each
+                cooling time in the ALARADFrame.
         '''
 
-        try:
-            totals = self.filter_rows({'nuclide : total'})['value'].tolist()
+        # Case 1: Pivoted ALARADFrame with "total" row
+        if self.index.name == 'nuclide' and 'total' in self.index:
+            totals = self[self.index == 'total'].iloc[0]
         
-        except AttributeError:
-            totals = self[self.index == 'total'].iloc[0].tolist()
-
+        # Case 2: Standard ALARADFrame with "total" rows
         else:
-           warn('ALARADFrame missing "total" row.')
-           totals = []
-        
-        return totals
+            totals = self.filter_rows({'nuclide' : 'total'})['value']
+
+        return totals.tolist()
 
 
 class DataLibrary:
@@ -417,52 +464,6 @@ class DataLibrary:
         return self.adf
 
 ###########################################
-
-def aggregate_small_percentages(piv, relative=False, threshold=0.05):
-    '''
-    Consolidate all rows in a pre-filtered and pivoted DataFrame derived from
-        a larger ALARADFRame that do not have any cells with a contribution of
-        more than a threshold value to the total for its respective column.
-        Rows that do not have any cells that surpass its column's threshold
-        are aggregated into a new "Other" row. If a row has at least one
-        column value above the threshold, then the whole row is preserved.
-
-    Arguments:
-        piv (pd.DataFrame): DataFrame created from the pivoting of an
-            ALARADFrame already operated on by ALARADFrame.filter_rows().
-        relative (bool, optional): Option for ALARADFrames already
-            processed by relative_contributions().
-            (Defaults to False)     
-        threshold (float or int): Proportional threshold value for
-            inclusion cutoff.
-            (Defaults to 0.05)
-
-    Returns:
-        agg (alara_output_processing.ALARADFrame): Processed ALARADFrame
-            (potentially) with new row, "Other", containing all aggregated
-            data below the thresholds.
-    '''
-
-    piv = ALARADFrame(piv)
-    cols = piv.columns
-    
-    rel_adjustment = (
-        [1] * len(cols) if relative else piv.extract_totals()
-    )
-    threshold_vals = {
-        col: threshold * adj for col, adj in zip(cols, rel_adjustment)
-    }
-    piv = piv[piv.index != 'total']
-
-    small_mask = (piv[cols] < pd.Series(threshold_vals)).all(axis=1)
-    other_row = pd.Series(piv.loc[small_mask, cols].sum(), name='Other')
-    agg = piv.loc[~small_mask.values]
-
-    if other_row[cols].sum() > 0:
-        agg = pd.concat([agg, other_row.to_frame().T])
-    agg.index.name = 'nuclide'
-
-    return agg
 
 def args():
     argparser = argparse.ArgumentParser()
