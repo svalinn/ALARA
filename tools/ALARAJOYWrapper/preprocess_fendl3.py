c6d991cdd3cf09ebdc6d7ba526e5b8c65e0c8171
Eitan Shai Weinstein <esweinstein2@tux-120.cae.wisc.edu>
2025-12-08 09:10:44 -0600

    Changing logic to filter on cumulative_data as a whole, rather than per reaction

--- tools/ALARAJOYWrapper/preprocess_fendl3.py
+++ tools/ALARAJOYWrapper/preprocess_fendl3.py
@@ -20,28 +20,34 @@ def args():
     )
     return parser.parse_args()
 
-def remove_gas_daughters(rxn):
-    '''
-    Remove all reactions resulting in a light gas daughter. Optional
-        method to be called ahead of data writing to file in write_dsv().
+def remove_gas_daughters(cumulative_data):
+    """
+    Filter reactions from the list of all reaction row dictionaries to remove
+        all reactions resulting in a light gas daughter. Optional method to be
+        called within gas_handling().
 
     Arguments:
-        rxn (dict): Dictionary containing a row of reaction data for each
-            reaction pathway, containing the groupwise cross-section data.
+        cumulative_data (list of dicts): List containing separate dictionaries
+            for each reaction contained in all of the TENDL/PENDF files
+            processed.
 
     Returns:
-        is_not_gas_daug (bool): Boolean condition to determine whether or not 
-            a reaction will be written out to the DSV based on if the daughter
-            is either heavier than an alpha particle (i.e. not a light gas) or
-            if the reaction represents any of the MT = 203-207 total gas
-            production reactions (as denoted by the 'x' emitted particle).
-    '''
+        gas_filtered (list of dicts): List of reactions that satisfy the
+            conditions that the daughter nuclide is either heavier than an
+            alpha particle (i.e. not a light gas) or that the reaction
+            represents any of the MT = 203-207 total gas production reactions
+            (as denoted by the 'x' emitted particle).
+    """
 
     alpha_kza = 20040 # Heaviest gas in interest alpha-particle
-    return rxn['Daughter KZA'] > alpha_kza or rxn['Emitted Particles'] == 'x'
 
-def gas_handling(gas_method, rxn):
-    '''
+    return [
+        rxn for rxn in cumulative_data
+        if rxn['Daughter KZA'] > alpha_kza or rxn['Emitted Particles'] == 'x'
+    ]
+
+def gas_handling(gas_method, cumulative_data):
+    """
     Set handling method for gas production total cross-sections for any given
         reaction to determine whether it will be written out to the DSV or 
         not. Either remove_gas_daughters() or subtract_gas_from_totals()
@@ -52,24 +58,24 @@ def gas_handling(gas_method, rxn):
         gas_method (str): Choice of method for handling gas production total
             cross-sections. Either 'r' (remove) or 's' (subtract). See
             ALARAJOYWrapper/README.md for futher details on these methods.
-        rxn (dict): Dictionary containing a row of reaction data for each
-            reaction pathway, containing the groupwise cross-section data.
+        cumulative_data (list of dicts): List containing separate dictionaries
+            for each reaction contained in all of the TENDL/PENDF files
+            processed.
 
     Returns:
-        write_out (bool): Boolean condition for whether a particular reaction
-            will be written out to the DSV, based on its matching of the given
-            gas handling method.
-    '''
+        gas_filtered (list of dicts): List of reactions that satisfy the
+            selected gas handling method.
+    """
 
     if gas_method == 'r':
-        return remove_gas_daughters(rxn)
+        return remove_gas_daughters(cumulative_data)
 
     # Pathway for subtraction method to be developed in a separate PR
     # to close #186
     # if gas_method == 's':
         # return subtract_gas_from_totals(rxn)
 
-def write_dsv(dsv_path, cumulative_data, gas_method):
+def write_dsv(dsv_path, row_dicts):
     """
     Write out a space-delimited DSV file from the list of dictionaries,
         dsv_path, produced by iterating through each reaction of each isotope
@@ -80,31 +86,30 @@ def write_dsv(dsv_path, cumulative_data, gas_method):
     Arguments:
         dsv_path (pathlib._local.PosixPath): Filepath for the DSV file to be
             written.
-        cumulative_data (list of dicts): List containing separate dictionaries
-            for each reaction contained in all of the TENDL/PENDF files
-            processed.
+        row_dicts (list of dicts): List containing separate dictionaries
+            for each reaction to be written out to the DSV.
+    
     Returns:
         None 
     """
 
     xs_key = 'Cross Sections'
-    join_keys = list(cumulative_data[0].keys())
+    join_keys = list(row_dicts[0].keys())
     join_keys.remove(xs_key)
     # Sort list of reaction dictionaries by ascending parent KZAs
     parent_label = join_keys[0]
-    cumulative_data.sort(key=lambda rxn: rxn[parent_label])
+    row_dicts.sort(key=lambda rxn: rxn[parent_label])
 
     with open(dsv_path, 'w') as dsv_file:
         # Write header line with total groups for Vitamin-J
         vitamin_J_energy_groups = 175
         dsv_file.write(str(vitamin_J_energy_groups) + '\n')
 
-        for reaction in cumulative_data:
-            if gas_handling(gas_method, reaction):
-                dsv_row = ' '.join(str(reaction[key]) for key in join_keys)
-                dsv_row += ' ' + ' '.join(str(xs) for xs in reaction[xs_key])
-                dsv_row += '\n'
-                dsv_file.write(dsv_row)
+        for reaction in row_dicts:
+            dsv_row = ' '.join(str(reaction[key]) for key in join_keys)
+            dsv_row += ' ' + ' '.join(str(xs) for xs in reaction[xs_key])
+            dsv_row += '\n'
+            dsv_file.write(dsv_row)
         # End of File (EOF) signifier to be read by ALARAJOY
         dsv_file.write(str(-1))
 
@@ -192,9 +197,11 @@ def main():
 
         njt.cleanup_njoy_files(element, A)
 
-    #remove_gas_daughters(cumulative_data)
+    # Handle gas total production cross-sections, per user specifications
+    gas_filtered = gas_handling(args().gas_handling[0], cumulative_data)
+    
     dsv_path = dir / 'cumulative_gendf_data.dsv'
-    write_dsv(dsv_path, cumulative_data, args().gas_handling[0])
+    write_dsv(dsv_path, gas_filtered)
     print(dsv_path)
 
 if __name__ == '__main__':
