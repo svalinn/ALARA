c9e9cedd083f4b3d4afa00bf933715a87f35fba9
Anupama Rajendra <asrajendra@wisc.edu>
2026-01-12 16:01:17 -0600

    fix formatting

--- src/Schedule.C
+++ src/Schedule.C
@@ -205,6 +205,7 @@ void Schedule::write(int level, char *histName, double delay, char dUnits)
       cout << "Schedule '" << ptr->schedName << "' with " << delay 
 	   << " " << dUnits << " delay and pulsed with history '" 
 	   << histName << "':" << endl;
+  cout << "\t";
       cout << "Schedule '" << ptr->schedName << "' pulse_history " << histName 
 	   << " delay " << delay << " " << dUnits << endl;
     }
