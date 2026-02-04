c78548cabcdd18193c96e0e76edd287eb4241fca
Anupama Rajendra <asrajendra@wisc.edu>
2026-02-04 11:05:04 -0600

    fix typo

--- tools/sched_post_processor.py
+++ tools/sched_post_processor.py
@@ -36,7 +36,7 @@ def make_nested_dict(lines):
         if line.startswith("pulse_history:"): #next section of output
             break
 
-        chile_level = line.count('\t')
+        child_level = line.count('\t')
         newline_name = line.lstrip('\t').rstrip('\n')
 
         if newline_name.strip().split()[0] == 'schedule':
