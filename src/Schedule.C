1537ca2385696942b16fb0db0bfd900d806a9130
Anupama Rajendra <asrajendra@wisc.edu>
2026-04-06 08:56:37 -0500

    remove commented lines

--- src/Schedule.C
+++ src/Schedule.C
@@ -184,7 +184,6 @@ void Schedule::write(int level, char *histName, double delay, char dUnits)
   /* on first call, search for top level schedule */
   if (level==0)
     {
-      // bool found_top_sched = FALSE;
       verbose(0,"\n\n***Please review this schedule hierarchy.!!!!!!!!!!\n");
       Schedule *top_sched = NULL;
       while (ptr->next != NULL)
@@ -192,16 +191,12 @@ void Schedule::write(int level, char *histName, double delay, char dUnits)
 	  ptr = ptr->next;
 	  if (!ptr->usedAsSub)
     {
-      // if (found_top_sched)
-      //   error(400, "Multiple top schedules have been found. Only one schedule can be the top schedule.");
-      // found_top_sched = TRUE;
       top_sched = ptr;
       break;
     }
     
 	}    
       if (top_sched == NULL)
-      // if (found_top_sched == FALSE)
 	      error(400,"Unable to find top level schedule.\nA top level schedule must not used as a sub-schedule.");
 
       Schedule *next_sched = ptr;
