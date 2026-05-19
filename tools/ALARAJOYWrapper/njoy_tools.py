aaf16207510ec5d89a7009932f47229bde7d49b5
Eitan Shai Weinstein <esweinstein2@tux-121.cae.wisc.edu>
2026-05-19 15:23:26 -0500

    Handling TENDL2014 UNRESR failures, invalid nuc names

--- tools/ALARAJOYWrapper/njoy_tools.py
+++ tools/ALARAJOYWrapper/njoy_tools.py
@@ -72,6 +72,17 @@ stop
     MATD = 0,                                # next mat number to be processed
 ))
 
+gaspr_input = Template(Template(
+"""
+gaspr
+ $NENDF $npend_gaspr $NOUT_gaspr/
+stop
+"""
+).safe_substitute(
+    NENDF = 21,                # unit for endf tape (equivalent to NOUT_moder)
+    NOUT_gaspr = 25,                      # unit for GASPR-produced pendf tape
+))
+
 groupr_input = Template(Template(
 """
 groupr/
@@ -355,7 +366,7 @@ def card9_special_isomer_reactions(pKZA, MT, mt_data, isomer_data, mtname):
 
 def fill_input_template(
     inp, material_id, MTs, element, A, mt_dict, temperature,
-    pKZA=None, isomer_dict={}, ign=17, ngn='', egn=''
+    pKZA=None, isomer_dict={}, unresr_fail=False, ign=17, ngn='', egn=''
 ):
     """
     Substitute in the material-specific values for a given ENDF/PENDF file
@@ -388,6 +399,10 @@ def fill_input_template(
             states of the residual daughter. Only needed when filling the
             GROUPR-specific template.
             (Defaults to {})
+        unresr_fail (bool, optional): Boolean flag to indicate if the UNRESR
+            module raised an ***error in rdunf2*** message (see NJOY module
+            section 5.6 for further description of UNRESR errors).
+            (Defaults to False)
         ign (str or int, optional): GROUPR neutron group structure parameter.
             ign = 1 for arbitrary group structures not contained in NJOY's
             built-in list of options. Default value corresponds to ign key for
@@ -437,6 +452,13 @@ def fill_input_template(
                 card9_lines.append(f'{MFD} {MT} "{mtname}" /')
 
     card9 = '\n '.join(card9_lines)
+    
+    # Set GASPR PENDF input. If UNRESR fails, apply the previous PENDF tape
+    # for GASPR input
+    npend_gaspr = 24
+    if unresr_fail:
+        npend_gaspr -= 1
+
     return inp.substitute(
         element=element,
         a=A,
@@ -449,6 +471,7 @@ def fill_input_template(
         ngn=ngn,
         egn=egn,                     
         reactions=card9,                                        
+        npend_gaspr=npend_gaspr
     )
 
 def write_njoy_input_file(template):
@@ -569,7 +592,8 @@ def run_njoy(element, A, matb, file_capture):
                                     File path to the newly created GENDF file.
                                     Returns None if NJOY runs unsuccessfuly.
         result.stderr (str or None): Output of NJOY error.
-                                    Returns None if NJOY runs successfully. 
+                                    Returns None if NJOY runs successfully.
+        result.stdout (str): NJOY standard output.
     """
 
     file_metadata = {
@@ -599,7 +623,7 @@ def run_njoy(element, A, matb, file_capture):
         if fileinfo['ext'] == '.gendf':
             ensure_gendf_markers(fileinfo['save'], matb)
             
-    return fileinfo['save'], result.stderr
+    return fileinfo['save'], result.stderr, result.stdout
 
 def copy_energy_groups_from_njoy_output(
     group_name, nGroups, groupr_output='output'
