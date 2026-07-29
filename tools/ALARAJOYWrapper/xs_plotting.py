07374422a8bbb48754fbd412c8f92c0b02efb612
Eitan Shai Weinstein <esweinstein2@linux-2620.cae.wisc.edu>
2026-07-29 12:10:25 -0500

    Migrating all ENDF interfacing to openmc.data

--- tools/ALARAJOYWrapper/xs_plotting.py
+++ tools/ALARAJOYWrapper/xs_plotting.py
@@ -88,7 +88,7 @@ def extract_continuous_data(endf_obj, MT):
 
     continuous_dict = {'energies' : [], 'xs' : []}
     MT, isomeric_state = flagged_num_to_int(MT)
-    rxn = Reaction.from_endf(endf_obj, MT)
+    rxn_obj = Reaction.from_endf(endf_obj, MT)
 
     # For excitation reactions, calculate specific pathway reactions by
     # multiplying reaction multiplicities by MF3 cumulative cross-sections
@@ -127,7 +127,7 @@ def extract_continuous_data(endf_obj, MT):
             # cumulative cross-sections and MF9 multiplicities
             if matched_MF == 9:
                 continuous_dict['xs'].extend(
-                    tab1.y * rxn.xs['0K'](energies)
+                    tab1.y * rxn_obj.xs['0K'](energies)
                 )
 
             # MF10 cross-sections can be extracted directly without need for
@@ -136,7 +136,7 @@ def extract_continuous_data(endf_obj, MT):
                 continuous_dict['xs'].extend(tab1.y)
 
     else:
-        mf3_xs_table = rxn.xs.get('0K')
+        mf3_xs_table = rxn_obj.xs.get('0K')
         if mf3_xs_table:
             continuous_dict['energies'].extend(mf3_xs_table.x)
             continuous_dict['xs'].extend(mf3_xs_table.y)
